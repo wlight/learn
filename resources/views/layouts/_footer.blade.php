8e1ce86013a1be6c10977bed7742c5f1feefa11c
wlight <wormside@gmail.com>
2023-10-25 22:55:37 +0800

    分割头部和尾部子视图

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <img class="brand-icon" src="https://cdn.learnku.com/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png" alt="">
+    <a href="#" target="_blank" class="text-decoration-none">
+        刻意练习，每日精进
+    </a>
+    <div class="float-end">
+        <a href="/about" class="text-decoration-none">关于</a>
+    </div>
+</footer>
