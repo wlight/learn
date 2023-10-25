3a8653645ebedf08591f2c1891efd94e3672905f
wlight <wormside@gmail.com>
2023-10-25 22:42:15 +0800

    初始化样式

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,9 +5,21 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>@yield('title', 'Learn Center')</title>
+    <title>@yield('title', 'Learn Center')- 我的学习中心</title>
+    <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
-    @yield('content')
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a href="/" class="navbar-brand">主页</a>
+            <ul class="navbar-nav justify-content-end">
+                <li class="nav-item"><a class="nav-link" href="/help">帮助</a></li>
+                <li class="nav-item"><a class="nav-link" href="/login">登录</a></li>
+            </ul>
+        </div>
+    </nav>
+    <div class="container">
+        @yield('content')
+    </div>
 </body>
 </html>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,5 +1,16 @@
 @extends('layouts.default')
 
 @section('content')
-    <h1>主页</h1>
+    <div class="bg-light p-3 p-sm-5 rounded">
+        <h1>Hello</h1>
+        <p class="lead">
+            你现在看到的是主页
+        </p>
+        <p>
+            一切将从这里开始。
+        </p>
+        <p>
+            <a class="btn btn-lg btn-success" href="#" role="button">现在注册</a>
+        </p>
+    </div>
 @stop
